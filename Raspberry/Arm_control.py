d6934e917bd1b7502dc31f8679412a0295492c4f
Youssef0Salah <youssufsalah008@gmail.com>
2024-09-05 17:59:10 +0300

    Added arm control

---
+++ Raspberry/Arm_control.py
@@ -0,0 +1,89 @@
+import board
+import busio
+import time
+import requests
+import adafruit_pca9685
+
+i2c = busio.I2C(board.SCL, board.SDA)
+pca = adafruit_pca9685.PCA9685(i2c, address=0x40)
+pca.frequency = 50
+
+AI_MODEL_IP = "http://192.168.43.89"
+AI_MODEL_PORT = "19999"
+
+past_distance = -1
+past_orientation = -1
+
+
+servos : dict = {"Base":0, "Elbow":1, "Spin" : 2, "Gripper":3,"Shoulder":4}
+
+# Function to stop (lock) the servo
+def lock_servo(channel):
+    neutral_duty_cycle = 0x8000 
+    pca.channels[channel].duty_cycle = neutral_duty_cycle
+
+
+def move_servo_clockwise(channel, pulse):
+    pca.channels[channel].duty_cycle = 0x1200
+    time.sleep(pulse)
+    lock_servo(channel)
+
+def move_servo_counter_clockwise(channel, pulse):
+    pca.channels[channel].duty_cycle = 0x17FF
+    time.sleep(pulse)
+    lock_servo(channel)
+
+
+def post_image(image):
+    try:
+        response = requests.post(f"{AI_MODEL_IP}:{AI_MODEL_PORT}/", data={"imageFile":image})
+        return response.text
+    except:
+        return "-1|-1\n"
+
+
+def move_arm(response: str):
+    new_distance, new_ortientation = response.split("|")
+
+    if int(new_distance) == -1 and int(new_ortientation) == -1:
+        return
+    if int(new_distance) == 0 and int(new_ortientation) == 0:
+        if past_orientation < 280:
+            move_servo_clockwise(servos["Base"], 0.15)
+        if past_orientation > 430:
+            move_servo_counter_clockwise(servos["Base"], 0.15)
+        else:
+            # Add reset code here
+            pass
+    else:
+        if int(new_distance) < 5:
+            move_servo_clockwise(servos["Gripper"],0.2)
+        if int(new_ortientation) < 280:
+            move_servo_counter_clockwise(servos["Base"],0.2)
+        if int(new_ortientation) > 430:
+            move_servo_clockwise(servos['Base'],0.2)
+
+        if int(new_distance) > 5:
+            distance = int(new_distance)
+            # Check the pulse-time sent, must be configured manually
+            move_servo_clockwise(servos["Shoulder"],0.2)
+            move_servo_clockwise(servos["Elbow"], 0.2)
+
+        global past_distance, past_orientation
+        past_orientation, past_distance = int(past_orientation), int(past_distance)
+
+
+def capture_image():
+    #  Needs raspberry pi-cam code
+    pass
+
+
+def start_operations(sampling_rate):
+    while True:
+        try:
+            image = capture_image()
+            response = post_image(image)
+            move_arm(response)
+            time.sleep(sampling_rate)
+        except Exception as e:
+            print("An exception occured and could not be dealt with,", e)
