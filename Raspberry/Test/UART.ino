6f16f94feabd3fa61c794f9d134264dd4bca7eb9
Diesaster <108914826+Diesaster@users.noreply.github.com>
2024-09-05 17:06:14 +0300

    Update UART.ino

--- Raspberry/Test/UART.ino
+++ Raspberry/Test/UART.ino
@@ -1,30 +1,28 @@
 #include <HardwareSerial.h>
 #include <Keypad.h>
 
-// Define GPIO pins
 const int ROWS = 4;
 const int COLS = 4;
 const int buzzer_pin = 14;
 const int speaker_first_pin = 4;
 const int speaker_second_pin = 2;
 
-// Keypad configuration
 char keys[ROWS][COLS] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
-byte rowPins[ROWS] = {34, 35, 32, 33};  // Connect to the row pins of the keypad
-byte colPins[COLS] = {25, 26, 27, 14};  // Connect to the column pins of the keypad
+byte rowPins[ROWS] = {34, 35, 32, 33};
+byte colPins[COLS] = {25, 26, 27, 14};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-HardwareSerial mySerial(1); // Use UART1 for communication
+HardwareSerial mySerial(1);
 
 void setup() {
   mySerial.begin(115200, SERIAL_8N1, 16, 17); // RX, TX pins
-  Serial.begin(115200); // For debugging
+  Serial.begin(115200);
 
   pinMode(buzzer_pin, OUTPUT);
   pinMode(speaker_first_pin, OUTPUT);
@@ -42,25 +40,38 @@ void loop() {
     mySerial.println(key);
   }
 
-  // Check if data is available on UART
   if (mySerial.available()) {
-    char command = (char)mySerial.read(); // Read the command
+    char command = (char)mySerial.read();
     if (command == 'B') {
-      digitalWrite(buzzer_pin, HIGH); // Set GPIO pin state
+      long_beep()
     } else if (command == 'b') {
-      digitalWrite(buzzer_pin, LOW);
+      short_beep()
     } else if (command == 'S') {
       String message = "";
       while (mySerial.available()) {
         char incomingChar = (char)mySerial.read();
-        if (incomingChar == '\n') { // End of the message
+        if (incomingChar == '\n') {
           break;
         }
-        message += incomingChar; // Append character to message
+        message += incomingChar;
       }
       // speaker_handler(message); // Process the message for the speaker
     }
   }
+}
+
+void short_beep() {
+  digitalWrite(buzzer_pin, HIGH);
+  delay(250);
+  digitalWrite(buzzer_pin, LOW);
+  delay(200);
+}
+
+void long_beep() {
+  digitalWrite(buzzer_pin, HIGH);
+  delay(500);
+  digitalWrite(buzzer_pin, LOW);
+  delay(200);
 }
 
 // void speaker_handler()
