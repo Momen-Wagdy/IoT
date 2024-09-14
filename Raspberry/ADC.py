f127009a912cf1c05d5dc9b3067ab2555e8996d5
Youssef Salah <youssufsalah008@gmail.com>
2024-09-14 11:29:03 +0300

    Update ADC.py

--- Raspberry/ADC.py
+++ Raspberry/ADC.py
@@ -1,71 +1,5 @@
-import RPi.GPIO as GPIO
-import time
+from flask import Flask, jsonify, request
 import threading
-import rospy
-from std_msgs.msg import String, Bool
-
-# Setup GPIO mode
-GPIO.setmode(GPIO.BCM)
-
-# Define GPIO pins
-data_pins = [8, 18, 23, 24, 25, 16, 12, 7]  # D0 to D7
-addr_pins = [13, 19, 26]  # A, B, C
-start_pin = 6  # START
-eoc_pin = 21  # EOC
-oe_pin = 5  # OE
-ale_pin = 22  # ALE
-clk_pin = 20  # CLK
-
-# Setup GPIO pins
-GPIO.setup(data_pins, GPIO.IN)
-GPIO.setup(addr_pins, GPIO.OUT)
-GPIO.setup(start_pin, GPIO.OUT)
-GPIO.setup(eoc_pin, GPIO.IN)
-GPIO.setup(oe_pin, GPIO.OUT)
-GPIO.setup(ale_pin, GPIO.OUT)
-GPIO.setup(clk_pin, GPIO.OUT)
-
-def generate_clock():
-    while True:
-        GPIO.output(clk_pin, GPIO.HIGH)
-        time.sleep(0.000005)  # Adjust this value for the clock frequency
-        GPIO.output(clk_pin, GPIO.LOW)
-        time.sleep(0.000005)
-
-def read_adc(channel):
-    # Select the channel
-    for i in range(3):
-        GPIO.output(addr_pins[i], (channel >> i) & 1)
-    
-    # Latch the address
-    GPIO.output(ale_pin, GPIO.HIGH)
-    time.sleep(0.001)  # Small delay
-    GPIO.output(ale_pin, GPIO.LOW)
-    
-    # Start conversion
-    GPIO.output(start_pin, GPIO.HIGH)
-    time.sleep(0.001)
-    GPIO.output(start_pin, GPIO.LOW)
-    
-    rospy.loginfo("Starting conversion.....")
-    # Wait for end of conversion
-    while GPIO.input(eoc_pin) == GPIO.HIGH:
-        time.sleep(0.01)  # Small delay to avoid tight loop
-    rospy.loginfo("Conversion done.")
-    
-    # Enable output
-    GPIO.output(oe_pin, GPIO.HIGH)
-    
-    # Read the 8-bit data
-    value = 0
-    for i in range(8):
-        if GPIO.input(data_pins[i]):
-            value |= (1 << i)
-    
-    # Disable output
-    GPIO.output(oe_pin, GPIO.LOW)
-    
-    return value
 
 class ADC_Node:
     def __init__(self):
@@ -75,10 +9,16 @@ class ADC_Node:
         self.power_pub = rospy.Publisher('power_off', Bool, queue_size=10)
         self.rate = rospy.Rate(10)
         self.buzzer_sequence = ""
+        self.sensor_data = {}  # Dictionary to hold sensor data
 
         # Start the clock generation in a separate thread
         clock_thread = threading.Thread(target=generate_clock)
         clock_thread.start()
+
+        # Start the Flask server in a separate thread
+        flask_thread = threading.Thread(target=self.start_flask_server)
+        flask_thread.daemon = True
+        flask_thread.start()
 
     def run(self):
         while not rospy.is_shutdown():
@@ -105,6 +45,7 @@ class ADC_Node:
         vout = (result*5 / 175)
         vin = vout*(7500/(30000+7500))*5
         rospy.loginfo(f"Channel {ch}: {vin}")
+        self.sensor_data['mq_2'] = vin
         if (vin > 100):
             self.buzzer_sequence += "B"
         else:
@@ -115,6 +56,7 @@ class ADC_Node:
         vout = (result*5 / 175)
         vin = vout*(7500/(30000+7500))*5
         rospy.loginfo(f"Channel {ch}: {vin}")
+        self.sensor_data['mq_9'] = vin
         if (vin > 100):
             self.buzzer_sequence += "B"
         else:
@@ -125,6 +67,7 @@ class ADC_Node:
         vout = (result*5 / 175)
         vin = vout*(7500/(30000+7500))*5
         rospy.loginfo(f"Channel {ch}: {vin}")
+        self.sensor_data['flame'] = vin
         if (vin > 100):
             self.buzzer_sequence += "B"
             self.flame_pub.publish(False)
@@ -137,6 +80,7 @@ class ADC_Node:
         v = result * (5/255)
         current = (v - 2.5) / 0.066
         rospy.loginfo(f"Channel {ch}: {current}")
+        self.sensor_data['current'] = current
         if (current > 100):
             self.power_pub.publish(False)
         else:
@@ -147,10 +91,20 @@ class ADC_Node:
         vout = (result*5 / 175)
         vin = vout*(7500/(30000+7500))*5
         rospy.loginfo(f"Channel {ch}: {vin}")
+        self.sensor_data['voltage'] = vin
         if (vin > 100):
             self.power_pub.publish(False)
         else:
             self.power_pub.publish(True)
+
+    def start_flask_server(self):
+        app = Flask(__name__)
+
+        @app.route('/sensor_data', methods=['GET'])
+        def get_sensor_data():
+            return jsonify(self.sensor_data)
+
+        app.run(host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     try:
