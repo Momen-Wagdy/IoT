00e30040078f3611dd72467fe9a8438fbd2bebea
Diesaster <108914826+Diesaster@users.noreply.github.com>
2024-09-14 15:16:34 +0300

    Update relay.py

--- Raspberry/relay.py
+++ Raspberry/relay.py
@@ -6,14 +6,20 @@ import time
 # Define GPIO pins for the relay channels
 relay_pins = [4, 17, 27, 10]  # First pin for immediate control, second pin for waterpump signal
 
-def waterpump_callback(msg):
+def waterpump_callback(data):
     # Callback function to control the second relay based on waterpump signal
-    if msg.data:
+    if data.data:
         GPIO.output(relay_pins[0], GPIO.LOW)  # Turn second relay (waterpump) on
         rospy.loginfo("Waterpump relay on: GPIO {}".format(relay_pins[0]))
     else:
         GPIO.output(relay_pins[0], GPIO.HIGH)  # Turn second relay (waterpump) off
         rospy.loginfo("Waterpump relay off: GPIO {}".format(relay_pins[0]))
+
+def fan_callback(data):
+    if data.data:
+        GPIO.output(relay_pins[1], GPIO.LOW)
+    else:
+        GPIO.output(relay_pins[1], GPIO.HIGH)
 
 def power_callback(data):
     if data.data:
@@ -36,9 +42,11 @@ def relay_control():
     relay_pub = rospy.Publisher('relay_status', String, queue_size=10)
 
     # Subscriber to listen for waterpump signal and control the second relay
-    rospy.Subscriber('waterpump_signal', Bool, waterpump_callback)
+    water_sub = rospy.Subscriber('waterpump_signal', Bool, waterpump_callback)
 
     relay_sub = rospy.Subscriber('power', Bool, power_callback)
+
+    fan_sub = rospy.subscriber('fan', Bool, fan_callback)
 
 
     try:
